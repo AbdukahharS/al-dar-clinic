a79e8768139052a14a61d0d5e396c99a5a80622a
AbdukahharS <shakhzodbekkakhkhorov04@gmail.com>
2025-01-02 03:18:04 +0800

    Refactor app layout to include authentication checks for admin routes and streamline component rendering

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
+import { useEffect } from 'react'
 import { Provider } from 'react-redux'
 import { Toaster } from 'react-hot-toast'
-import { usePathname } from 'next/navigation'
+import { usePathname, useRouter } from 'next/navigation'
 
 import store from '../redux/store'
 import Navbar from '@/components/layout/Navbar'
@@ -8,31 +9,50 @@ import Footer from '@/components/layout/Footer'
 import '@/styles/globals.css'
 import '@/styles/fonts.css'
 import SideBar from '@/components/SideBar'
+import useAuth from '@/hooks/useAuth'
 
 export default function App({ Component, pageProps }) {
-  const path = usePathname()
-
   return (
     <Provider store={store}>
       <Toaster />
-      <div className='min-h-screen flex flex-col'>
-        {!path?.startsWith('/admin') && <Navbar />}
-        <div className='overflow-hidden flex-1 bg-gradient-to-b from-[#f9f9f9] from-0% to-white to-20%'>
-          {path?.startsWith('/profile') ? (
-            <div className='w-full max-w-7xl mx-auto py-10 px-7 md:pt-16 flex flex-col md:flex-row md:items-start gap-8 md:gap-16'>
-              <SideBar />
-              <div className='flex-1'>
-                <Component {...pageProps} />
-              </div>
+      <Layout Component={Component} pageProps={pageProps} />
+    </Provider>
+  )
+}
+
+const Layout = ({ Component, pageProps }) => {
+  const path = usePathname()
+  const { isAuthenticated } = useAuth()
+  const router = useRouter()
+
+  useEffect(() => {
+    if (
+      !isAuthenticated &&
+      path.startsWith('/admin') &&
+      !path.startsWith('/admin/auth')
+    ) {
+      router.push('/admin/auth/login')
+    }
+
+    if (path === '/admin/auth') router.push('/admin/auth/login')
+  }, [path, isAuthenticated])
+
+  return (
+    <div className='min-h-screen flex flex-col'>
+      {path?.startsWith('/admin') ? null : <Navbar />}
+      <div className='overflow-hidden flex-1 bg-gradient-to-b from-[#f9f9f9] from-0% to-white to-20%'>
+        {path?.startsWith('/profile') ? (
+          <div className='w-full max-w-7xl mx-auto py-10 px-7 md:pt-16 flex flex-col md:flex-row md:items-start gap-8 md:gap-16'>
+            <SideBar />
+            <div className='flex-1'>
+              <Component {...pageProps} />
             </div>
-          ) : (
-            <Component {...pageProps} />
-          )}
-        </div>
-        {!path?.startsWith('/auth') && !path?.startsWith('/admin') && (
-          <Footer />
+          </div>
+        ) : (
+          <Component {...pageProps} />
         )}
       </div>
-    </Provider>
+      {!(path?.startsWith('/auth') || path?.startsWith('/admin')) && <Footer />}
+    </div>
   )
 }
