33078f7c8306ec205480911205741083a9d2b82c
AbdukahharS <shakhzodbekkakhkhorov04@gmail.com>
2025-01-09 19:01:04 +0800

    Enhance location management: implement add/edit functionality, add confirmation dialog for delete actions, and improve form validation

--- pages/admin/location/index.jsx
+++ pages/admin/location/index.jsx
@@ -1,135 +1,142 @@
 import { useState } from 'react'
-import Link from 'next/link'
-import { FaPen, FaTrashCan } from 'react-icons/fa6'
-import Image from 'next/image'
+import { FaPen, FaTrash, FaX } from 'react-icons/fa6'
+import { useForm } from 'react-hook-form'
+import { motion } from 'framer-motion'
+
 import Button from '@/components/Button'
+import confirm from '@/components/Confirm'
 
-import dubai from '@/public/images/dubai.webp'
-import dubai1 from '@/public/images/dubai-1.webp'
-import dubai2 from '@/public/images/dubai-2.webp'
-import dubai3 from '@/public/images/dubai-3.webp'
-import dubai4 from '@/public/images/dubai-4.webp'
-import oman from '@/public/images/oman.webp'
-import oman1 from '@/public/images/oman-1.webp'
-import oman2 from '@/public/images/oman-2.webp'
-import oman3 from '@/public/images/oman-3.webp'
-import oman4 from '@/public/images/oman-4.webp'
-import iraq from '@/public/images/iraq.webp'
-import iraq1 from '@/public/images/iraq-1.webp'
-import iraq2 from '@/public/images/iraq-2.webp'
-import iraq3 from '@/public/images/iraq-3.webp'
-import iraq4 from '@/public/images/iraq-4.webp'
-
-const dummyLocations = [
-  {
-    id: 1,
-    name: 'Dubai',
-    mainPicture: dubai,
-    pictures: [dubai1, dubai2, dubai3, dubai4],
-    telephone: '+971 4 123 4567',
-    email: 'dubai@example.com',
-  },
-  {
-    id: 2,
-    name: 'Oman',
-    mainPicture: oman,
-    pictures: [oman1, oman2, oman3, oman4],
-    telephone: '+968 24 123 456',
-    email: 'oman@example.com',
-  },
-  {
-    id: 3,
-    name: 'Iraq',
-    mainPicture: iraq,
-    pictures: [iraq1, iraq2, iraq3, iraq4],
-    telephone: '+964 1 123 4567',
-    email: 'iraq@example.com',
-  },
-]
+const dummyLocations = [{ name: 'Dubai' }, { name: 'Oman' }, { name: 'Iraq' }]
 
 const LocationManagement = () => {
   const [locations, setLocations] = useState(dummyLocations)
+  const [editIndex, setEditIndex] = useState(null)
+
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    getValues,
+    formState: { errors },
+  } = useForm()
+
+  const handleAdd = (data) => {
+    const newLocation = { name: data.locationName }
+    setLocations([...locations, newLocation])
+    setValue('locationName', null)
+  }
+
+  const handleDelete = async (index) => {
+    const removeFromList = () =>
+      setLocations(locations.filter((_, i) => i !== index))
+    confirm(
+      'Delete Location',
+      'Are you sure you want to delete this location?',
+      'Delete',
+      removeFromList
+    )
+  }
+
+  const handleEdit = (index) => {
+    if (!index) {
+      setEditIndex(null)
+      setValue('locationName', null)
+      return
+    }
+    setEditIndex(index)
+    const location = locations[index]
+    setValue('locationName', location.name)
+  }
+
+  const handleUpdate = (data) => {
+    const updatedLocation = { name: data.locationName }
+    const updatedLocations = locations.map((location, index) =>
+      index === editIndex ? updatedLocation : location
+    )
+    setLocations(updatedLocations)
+    setEditIndex(null)
+    setValue('locationName', null)
+  }
 
   return (
     <div>
       <div className='bg-primary text-white px-8 md:px-20 py-8 flex justify-between items-center'>
         <h1 className='text-2xl font-medium'>Location Management</h1>
-        <div className='flex items-center gap-4'>
-          <Link href='/admin/location/create'>
-            <Button className='bg-white !text-primary rounded-lg p-2'>
-              Add Location
-            </Button>
-          </Link>
-        </div>
       </div>
-      <div className='overflow-x-auto'>
-        <table className='min-w-full table-auto text-gray-700'>
-          <thead>
-            <tr>
-              <th className='px-4 py-5 font-medium whitespace-nowrap'>Name</th>
-              <th className='px-4 py-5 font-medium whitespace-nowrap'>
-                Main Picture
-              </th>
-              <th className='px-4 py-5 font-medium whitespace-nowrap'>
-                Other Pictures
-              </th>
-              <th className='px-4 py-5 font-medium whitespace-nowrap'>
-                Telephone
-              </th>
-              <th className='px-4 py-5 font-medium whitespace-nowrap'>Email</th>
-              <th className='px-4 py-5 font-medium whitespace-nowrap'>
-                Action
-              </th>
+
+      <table className='w-full max-w-lg table-auto mx-auto shadow-md mt-7'>
+        <thead>
+          <tr className='bg-gray-200'>
+            <th className='px-4 py-2 text-left'>Location Name</th>
+            <th className='px-4 py-2'>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {locations.map((location, index) => (
+            <tr key={index} className='border-b'>
+              <td className='px-4 py-2'>{location.name}</td>
+              <td className='px-4 py-2 flex gap-3 items-center justify-center'>
+                {editIndex === index ? (
+                  <button
+                    onClick={() => handleEdit()}
+                    className='text-red-500 hover:underline mr-2'
+                  >
+                    <FaX />
+                  </button>
+                ) : (
+                  <button
+                    onClick={() => handleEdit(index)}
+                    className='text-primary hover:underline mr-2'
+                  >
+                    <FaPen />
+                  </button>
+                )}
+                <button
+                  onClick={() => handleDelete(index)}
+                  className='text-primary hover:underline'
+                >
+                  <FaTrash />
+                </button>
+              </td>
             </tr>
-          </thead>
-          <tbody>
-            {locations.map((location, index) => (
-              <tr key={index} className='border'>
-                <td className='px-3 py-4 whitespace-nowrap text-center'>
-                  {location.name}
-                </td>
-                <td className='px-3 py-4 text-center whitespace-nowrap'>
-                  <Image
-                    src={location.mainPicture}
-                    alt={location.name}
-                    loading='lazy'
-                    className='w-32 h-24 rounded mx-auto'
-                  />
-                </td>
-                <td className='px-3 py-4 text-center whitespace-nowrap'>
-                  <div className='flex justify-center gap-2'>
-                    {location.pictures.map((pic, idx) => (
-                      <Image
-                        key={idx}
-                        src={pic}
-                        alt={`${location.name} ${idx + 1}`}
-                        loading='lazy'
-                        className='w-12 h-12 rounded-full'
-                      />
-                    ))}
-                  </div>
-                </td>
-                <td className='px-3 py-4 text-center whitespace-nowrap'>
-                  {location.telephone}
-                </td>
-                <td className='px-3 py-4 text-center whitespace-nowrap'>
-                  {location.email}
-                </td>
-                <td className='px-3 py-4 text-primary whitespace-nowrap'>
-                  <div className='flex items-center justify-center gap-3'>
-                    <Link href={`/admin/location/${location.id}`}>
-                      <FaPen className='mx-auto text-xl' />
-                    </Link>
-                    <button>
-                      <FaTrashCan className='mx-auto text-xl' />
-                    </button>
-                  </div>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
+          ))}
+        </tbody>
+      </table>
+
+      <form
+        className='mt-8 w-fit mx-auto flex flex-col gap-4'
+        onSubmit={handleSubmit(editIndex !== null ? handleUpdate : handleAdd)}
+      >
+        <h3 className='text-center text-primary text-2xl font-medium'>
+          {editIndex !== null ? 'Update Location' : 'Add New Location'}
+        </h3>
+        <div>
+          <label className='block text-lg font-medium text-gray-700'>
+            Location Name
+          </label>
+          <input
+            type='text'
+            {...register('locationName', {
+              required: 'Location Name is required',
+            })}
+            className={`mt-1 block w-full border p-2 ${
+              errors.locationName ? 'border-red-500' : 'border-gray-300'
+            } rounded-md shadow-s sm:text-sm`}
+          />
+          {errors.locationName && (
+            <motion.p
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              className='text-red-500 text-sm mt-1'
+            >
+              {errors.locationName.message}
+            </motion.p>
+          )}
+        </div>
+        <Button type='submit' className='!rounded-lg !px-12 mx-auto block'>
+          {editIndex !== null ? 'Update' : 'Add'}
+        </Button>
+      </form>
     </div>
   )
 }
