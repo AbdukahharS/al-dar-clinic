a0f6d75616843d6082f19457d603abbac1f0f138
AbdukahharS <shakhzodbekkakhkhorov04@gmail.com>
2025-01-15 11:34:26 +0800

    Remove console log from onSubmit function in EditProduct component

--- pages/admin/products/[productId]/edit.jsx
+++ pages/admin/products/[productId]/edit.jsx
@@ -98,8 +98,6 @@ const EditProduct = () => {
   }
 
   const onSubmit = async (data) => {
-    console.log(data)
-
     const { stockQuantity, price, weightInKg } = data
 
     const validationError = validateMultipleFields(
