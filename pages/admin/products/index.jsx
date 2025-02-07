ba095482239b130680ffb073524ea266868f6929
AbdukahharS <shakhzodbekkakhkhorov04@gmail.com>
2025-02-07 11:56:31 +0800

    Update padding in fixed header of Products page for improved layout

--- pages/admin/products/index.jsx
+++ pages/admin/products/index.jsx
@@ -95,7 +95,7 @@ const Products = () => {
       <div className='h-[104px]'></div>
 
       {/* Fixed header */}
-      <div className='bg-primary text-white px-8 md:px-20 py-8 flex justify-between items-center fixed top-[155px] md:top-0 w-full md:w-[calc(100%-288px)] z-10 right-0'>
+      <div className='bg-primary text-white px-6 xl:px-20 py-8 flex justify-between items-center fixed top-[155px] md:top-0 w-full md:w-[calc(100%-288px)] z-10 right-0'>
         <h1 className='text-2xl font-medium'>Product Management</h1>
         <div className='flex items-center gap-4'>
           <Button
